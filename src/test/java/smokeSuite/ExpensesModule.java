c08eedf2989c5705bffa9718bea7b61ec0589a7f
delal <delal633@gmail.com>
2019-07-18 22:11:31 -0500

    Delal's code

--- src/test/java/smokeSuite/ExpensesModule.java
+++ src/test/java/smokeSuite/ExpensesModule.java
@@ -1,6 +1,7 @@
 package smokeSuite;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
@@ -36,5 +37,35 @@ public class ExpensesModule {
         WebElement productTypeDropdown = Driver.getDriver().findElement(By.id("o_field_input_97"));
         Select select = new Select(productTypeDropdown);
         select.selectByVisibleText("Consumable");
+    }
+
+    @Test
+    public void uploadDc()throws InterruptedException{
+        Driver.getDriver().get(Config.getProperty("url"));
+        Thread.sleep(1000);
+        WebElement email  = Driver.getDriver().findElement(By.xpath("//div[@class='form-group field-login']//input"));
+        WebElement password = Driver.getDriver().findElement(By.xpath("//div[@class='form-group field-password']/input"));
+        email.sendKeys("in_ex_manager@info.com");
+        password.sendKeys("LLighg88");
+        Thread.sleep(1000);
+        WebElement logInButton = Driver.getDriver().findElement(By.xpath("//div[@class='clearfix oe_login_buttons']/button"));
+        logInButton.click();
+        WebElement Expenses = Driver.getDriver().findElement(By.xpath("//*[@id=\"oe_main_menu_navbar\"]/div[2]/ul[1]/li[9]/a/span"));
+        Expenses.click();
+        WebElement createButton = Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-primary btn-sm o_list_button_add']"));
+        createButton.click();
+        Driver.getDriver().findElement(By.id("o_field_input_19")).click();
+        Driver.getDriver().findElement(By.xpath("//a[.='[253] Tshirt']")).click();
+        Driver.getDriver().findElement(By.id("o_field_input_27")).click();
+        Driver.getDriver().findElement(By.xpath("//a[.='Ashley Presley']")).click();
+        Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-sm oe_stat_button']")).click();
+        Driver.getDriver().findElement(By.xpath("//button[@class='btn btn-sm btn-default o_button_import']")).click();
+        WebElement chooseFile = Driver.getDriver().findElement(By.xpath("//input[@class='oe_import_file_show form-control']"));
+        // chooseFile.click();
+        Thread.sleep(2000);
+
+        chooseFile.sendKeys("C:\\Users\\delal\\Desktop\\mavenb11\\briteERP.txt"+ Keys.ENTER);
+        Thread.sleep(2000);
+
     }
 }
