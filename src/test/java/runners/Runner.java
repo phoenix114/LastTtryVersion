ebf57d37d5f38fb3da6bc0e2fac9c2c095b18470
Ulan <kaltatay@gmail.com>
2019-08-07 22:54:32 -0500

    new version Jenkins

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,13 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-<<<<<<< HEAD
+
         plugin = "html:target/ExpensesModul",
         features = "src/test/resources/feature/expenses.feature",
-=======
-        plugin = "html:target/cucumber",
-        features = "src/test/resources/feature/",
->>>>>>> delal
+
         glue = "steps",
         tags = "@test9",
         dryRun = false
