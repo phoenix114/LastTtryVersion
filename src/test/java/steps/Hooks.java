e40251cb662056fd1fe4caa0d0aa76c8861da6f4
hilalkaya52 <hilalkaya52@gmail.com>
2019-08-08 22:29:28 -0500

    please end this

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,8 +1,8 @@
 package steps;
 
 import cucumber.api.Scenario;
-import org.junit.After;
-import org.junit.Before;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utilities.Config;
@@ -17,7 +17,7 @@ public void logInPage(){
 }
 
     @After
-<<<<<<< HEAD
+
     public void teardown(Scenario scenario){
         System.out.println("This is our hooks clean up");
         System.out.println(scenario.getName());
@@ -31,14 +31,5 @@ public void logInPage(){
 
     }
 }
-=======
-    public void tearDown(Scenario scenario){
-        if (scenario.isFailed()){
-            byte[] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot,"image/png");
-        }
 
-        Driver.quitDriver();
-    }
-}
->>>>>>> master
+
